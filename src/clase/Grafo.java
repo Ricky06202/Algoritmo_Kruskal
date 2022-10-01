7af0569b7015e82fa8851b480daeba994e3df82b
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2022-10-01 14:33:05 -0500

    Se hicieron las variables del Grafo

--- src/clase/Grafo.java
+++ src/clase/Grafo.java
@@ -3,5 +3,29 @@ package clase;
 // definir las caracteristicas(variables) y sus metodos(funciones)
 
 public class Grafo {
+    private Nodo[] nodos;
+    private Arista[] aristas;
+
+    public Grafo(Nodo[] nodos, Arista[] aristas) {
+        this.nodos = nodos;
+        this.aristas = aristas;
+    }
+
+    public Nodo[] getNodos() {
+        return nodos;
+    }
+
+    public void setNodos(Nodo[] nodos) {
+        this.nodos = nodos;
+    }
+
+    public Arista[] getAristas() {
+        return aristas;
+    }
+
+    public void setAristas(Arista[] aristas) {
+        this.aristas = aristas;
+    }
+
     
 }
