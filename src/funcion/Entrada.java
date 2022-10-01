874091f40d0b4c1b49cfb24a1800857734bed8e9
Ekkology <normales01@gmail.com>
2022-10-01 15:01:40 -0500

    Agregado de Funciones
    7u7

--- src/funcion/Entrada.java
+++ src/funcion/Entrada.java
@@ -1,8 +1,20 @@
 package funcion;
-
+import javax.swing.JOptionPane;
 // aki se van a hacer las funciones para recibir datos
 // mediante JOptionPane
 
 public class Entrada {
+    public static float pedirFloat(String Texto, String Titulo )
+    {    
+        String valFloat =  JOptionPane.showInputDialog(null,Texto, Titulo, JOptionPane.INFORMATION_MESSAGE);
+        return Float.parseFloat(valFloat);     
+
+    }
+    public static String pedirString(String Texto, String Titulo )
+    {    
+        String valString =  JOptionPane.showInputDialog(null,Texto, Titulo, JOptionPane.INFORMATION_MESSAGE);
+        return valString;     
+
+    }
     
 }
--- src/funcion/Salida.java
+++ src/funcion/Salida.java
@@ -1,8 +1,13 @@
 package funcion;
-
+import javax.swing.JOptionPane;
 // aki se van a hacer las funciones para mostrar datos
 // mediante JOptionPane
 
 public class Salida {
+
+    public static void Mensaje(String Texto, String Titulo)
+    {    
+        JOptionPane.showMessageDialog(null,Texto,Titulo,JOptionPane.INFORMATION_MESSAGE);
+    }
     
 }
