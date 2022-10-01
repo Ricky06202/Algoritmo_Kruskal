8480b35152a3f23786598451a0827295b39d1418
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2022-10-01 15:27:32 -0500

    mejorando las funciones de entrada y salida

--- src/funcion/Salida.java
+++ src/funcion/Salida.java
@@ -1,8 +1,16 @@
 package funcion;
-
+import javax.swing.JOptionPane;
 // aki se van a hacer las funciones para mostrar datos
 // mediante JOptionPane
 
 public class Salida {
-    
+    public static void Mensaje(String texto, String titulo)
+    {    
+        JOptionPane.showMessageDialog(null,texto,titulo,JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void MensajeError(String texto, String titulo)
+    {    
+        JOptionPane.showMessageDialog(null,texto,titulo,JOptionPane.ERROR_MESSAGE);
+    }
 }
