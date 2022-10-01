26802f8c8785f8ddafe9d1b22067bda10de1646f
Ricardo A Sanjur G <91886986+Ricky06202@users.noreply.github.com>
2022-10-01 16:53:20 -0500

    Ricardo (#13)
    * Mejoramos la estructura del Grafo
    
    * class actualizada del grafo

--- src/clase/Grafo.java
+++ src/clase/Grafo.java
@@ -5,29 +5,83 @@ import java.util.ArrayList;
 // definir las caracteristicas(variables) y sus metodos(funciones)
 
 public class Grafo {
-    private ArrayList<Nodo> nodos;
-    private ArrayList<Arista> aristas;
+    private int cantidadNodos = 0;
+    private ArrayList<Nodo> nodos = new ArrayList<Nodo>();
+    private ArrayList<Arista> aristas = new ArrayList<Arista>();
 
-    public Grafo(ArrayList<Nodo>  nodos, ArrayList<Arista> aristas) {
-        this.nodos = nodos;
-        this.aristas = aristas;
+    //!funciones para NODOS
+    public void agregarNodo(Nodo nodo){
+        if(!existeNodo(nodo)){
+            nodos.add(nodo);
+            cantidadNodos++;
+        }
     }
 
-    public ArrayList<Nodo>  getNodos() {
-        return nodos;
+    public void quitarNodo(Nodo nodo){
+        if(existeNodo(nodo)){
+            nodos.remove(nodo);
+            cantidadNodos--;
+        }
     }
 
-    public void setNodos(ArrayList<Nodo>  nodos) {
-        this.nodos = nodos;
+    public void quitarPrimerNodo(){
+        if(nodos.size() > 0){
+            nodos.remove(0);
+            cantidadNodos--;
+        }
     }
 
-    public ArrayList<Arista> getAristas() {
-        return aristas;
+    public void quitarUltimoNodo(){
+        if(nodos.size() > 0){
+            nodos.remove(nodos.size()-1);
+            cantidadNodos--;
+        }
     }
 
-    public void setAristas(ArrayList<Arista> aristas) {
-        this.aristas = aristas;
+    private boolean existeNodo(Nodo actual){
+        for (Nodo nodo : nodos) {
+            if(nodo.equals(actual))
+                return true;
+        }
+        return false;
     }
 
+    //!Funciones para ARISTAS
+    public void agregarArista(Arista arista){
+        if(!existeArista(arista)){
+            aristas.add(arista);
+            agregarNodosFaltantes(arista);
+        }
+    }
+
+    public void quitarArista(Arista arista){
+        if(existeArista(arista))
+            aristas.remove(arista);
+    }
+
+    public void quitarPrimerArista(){
+        if(aristas.size() > 0)
+            aristas.remove(0);
+    }
+
+    public void quitarUltimoArista(){
+        if(aristas.size() > 0)
+            aristas.remove(aristas.size()-1);
+    }
     
+    private boolean existeArista(Arista actual){
+        for (Arista arista : aristas) {
+            if(arista.equals(actual))
+                return true;
+        }
+        return false;
+    }
+
+    private void agregarNodosFaltantes(Arista arista){
+        if(!existeNodo(arista.getNodo1()))
+            agregarNodo(arista.getNodo1());
+        if(!existeNodo(arista.getNodo2()))
+            agregarNodo(arista.getNodo2());
+    }
+
 }
