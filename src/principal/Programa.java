d7a2a94ff840e9e1ebdce62d217bd1878823c66e
Ricardo A Sanjur G <91886986+Ricky06202@users.noreply.github.com>
2022-10-04 14:33:04 -0500

    Refactorizamos el programa en funciones
    para tener mejor comprencion

--- src/principal/Programa.java
+++ src/principal/Programa.java
@@ -1,5 +1,6 @@
 package principal;
 
+import clase.Arista;
 import clase.Grafo;
 import clase.Nodo;
 import funcion.Entrada;
@@ -8,21 +9,30 @@ import funcion.Entrada;
 public class Programa {
     public static void main(String[] args) 
     {
-        Grafo grafo = new Grafo();
+        algoritmoDeKruskal();
+    }
 
-        int opcion = Entrada.opcionMultiple("¿Qué desea agregar?","Creación del Grafo", "Nodo,Arista,Terminar".split(","));
+    private static void algoritmoDeKruskal() {
+        while (true) {
+            Grafo grafo = new Grafo();
+            int opcion = elegirOpcion();
+            realizarAccion(grafo, opcion);
+        }
+    }
 
+    private static int elegirOpcion() {
+        return Entrada.opcionMultiple("¿Qué desea agregar?","Creación del Grafo", "Nodo,Arista,Terminar".split(","));
+    }
+
+    private static void realizarAccion(Grafo grafo, int opcion) {
         switch (opcion) 
         {
             case 0: //Nodo
-                grafo.agregar(crearNodo());
+                agregarNodoAlGrafo(grafo);
                 break;
 
             case 1: //Arista
-                
-
-            
-                
+                agregarAristaAlGrafo(grafo);
                 break;
             case 2: //Terminar
             
@@ -30,10 +40,26 @@ public class Programa {
                 break;
         
             default: // Salir 
+                verificarSalida();
                 break;
         }
-        
+    }
 
+    private static void verificarSalida() {
+        boolean deseaSalir = Entrada.opcionSiNo("Seguro que desea salir?", "Confirmacion de Finalizacion del Porgrama");
+        if(deseaSalir)
+            System.exit(0);
+    }
+
+    private static void agregarAristaAlGrafo(Grafo grafo) {
+        Nodo nodo1 = crearNodo("primer","de la Arista");
+        Nodo nodo2 = crearNodo("segundo","de la Arista");
+        float longitud = Entrada.pedirNumeroPositivo("Ingrese la longitud entre los nodos", "Ingresar Longitud de la Arista");
+        grafo.agregar(new Arista(nodo1, nodo2, longitud));
+    }
+
+    private static void agregarNodoAlGrafo(Grafo grafo) {
+        grafo.agregar(crearNodo());
     }
 
     public static Nodo crearNodo()
